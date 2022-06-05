cea62415c6cde1010851690701dc63ad0cc30478
TheRefraction <106912968+TheRefraction@users.noreply.github.com>
2022-06-05 13:56:18 +0200

    Initial commit

---
+++ drawdownloadmodal.js
@@ -0,0 +1,20 @@
+document.write(`\
+	<link rel="stylesheet" href="downloadmodal.css">\
+	<div id="downloadBox" class="modal">\
+		<div class="modal-content">\
+			<div class="modal-header">\
+				<span class="close">&times;</span>\
+				<h2>Download DESTORY Demo</h2>\
+			</div>\
+			<div class="modal-body">\
+				<p><span class="fa fa-windows"></span>&nbsp&nbspWindows <button class="w3-button w3-round w3-right">Download</button></p>\
+				<p><span class="fa fa-linux"></span>&nbsp&nbspLinux <button class="w3-button w3-round w3-right">Download</button></p>\
+				<p><span class="fa fa-apple"></span>&nbsp&nbspMacOSX <button class="w3-button w3-round w3-right w3-disabled">Unavailable</button></p>\
+			</div>\
+			<!--<div class="modal-footer">\
+				<h3>Unused SECTION</h3>\
+			</div>-->\
+		</div>\
+	</div>\
+\
+`);
---
+++ drawfooter.js
@@ -0,0 +1,8 @@
+document.write(`\
+<footer class="w3-container w3-padding-64 w3-center w3-black w3-xlarge">\
+	<p class="w3-medium">\
+		<em>DESTORY<br>created and developed by<br>Corentin Hautefaye.<br><br>All Rights belong to Corentin Hautefaye<br>and to legitimate Developers.<br><br>Copyright (C) 2023 Corentin Hautefaye</em>\
+	</p>\
+</footer>\
+\
+`);
