b3e7badd8df0a37b5029d670b2384697f2e4b835
TheRefraction <106912968+TheRefraction@users.noreply.github.com>
2022-06-05 16:57:21 +0200

    Update drawdownloadmodal.js

--- drawdownloadmodal.js
+++ drawdownloadmodal.js
@@ -7,7 +7,7 @@ document.write(`\
 				<h2>Download DESTORY Demo</h2>\
 			</div>\
 			<div class="modal-body">\
-				<p><span class="fa fa-windows"></span>&nbsp&nbspWindows <button class="w3-button w3-round w3-right" href="downloads/testfile.exe" download="DESDEMO_Windows.exe">Download</button></p>\
+				<p><span class="fa fa-windows"></span>&nbsp&nbspWindows <button class="w3-button w3-round w3-right" href="/downloads/testfile.exe" download="DESDEMO_Windows.exe">Download</button></p>\
 				<p><span class="fa fa-linux"></span>&nbsp&nbspLinux <button class="w3-button w3-round w3-right w3-disabled">Unavailable</button></p>\
 				<p><span class="fa fa-apple"></span>&nbsp&nbspMacOSX <button class="w3-button w3-round w3-right w3-disabled">Unavailable</button></p>\
 			</div>\
